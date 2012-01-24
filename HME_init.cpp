670036fdd7ebbe526e81b901185f618f41a7a45d
DragoonXen <dragoon6@yandex.ru>
2012-01-24 22:55:21 +0300

    testCommit

---
+++ HME_init.cpp
@@ -0,0 +1,12 @@
+#include <fstream>
+#include <iostream>
+#include <cstring>
+#include <assert.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	ifstream f(argv[argc - 1]);
+	return 0;
+}
