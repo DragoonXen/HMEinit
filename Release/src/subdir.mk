4fe3c45c5d1fd906927bde9af92e89efee92f2f9
DragoonXen <dragoon6@yandex.ru>
2012-01-25 23:20:53 +0300

    *.mk files

---
+++ Release/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/hme_init.cpp \
+../src/regression_tree.cpp \
+../src/tree_node.cpp 
+
+OBJS += \
+./src/hme_init.o \
+./src/regression_tree.o \
+./src/tree_node.o 
+
+CPP_DEPS += \
+./src/hme_init.d \
+./src/regression_tree.d \
+./src/tree_node.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
